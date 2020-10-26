b3cb9cf92effba8ec8f732236d6b2151b2d8d84c
joao-vitor-machado <65265433+joao-vitor-machado@users.noreply.github.com>
2020-10-25 23:41:18 -0300

    edit 25/10

--- ex.py
+++ ex.py
@@ -1,4 +1,28 @@
 import random as rd
 
-nada = input(" seu nome \n")
-print(nada)
+# variáveis 
+
+sinalizadorVermmelho = False #define se o sinalizador será ativado ou não
+numeroMensageirosVermelhos = 5
+numeroMensageirosAzuis = 10
+tempo = 0 #em segundos
+
+
+#FUNÇÕES
+
+def enviarMensageiro (mensageiro, tempo, nMensageiros):
+    probabilidadeSerPego = rd.randrange(0,101)
+
+    if(probabilidadeSerPego <= 45):
+        return False, tempo, nMensageiros - 1 #Define se o mensageiro será morto ou não, o tempo atual e o número de mensageiros disponíveis
+    else:
+
+        tempo+= rd.randrange(3600, 4201)
+
+        return True, tempo
+
+
+
+#MAIN CODE
+
+    
